4286c9ff858283451e91eb32401fa2fbbcbdda6f
ankur8687 <58618180+ankur8687@users.noreply.github.com>
2026-01-26 16:39:05 +0530

    Fragment

---
+++ src/Fragment-React/Fragment.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Row from "./Row.jsx";
+function App(){
+    return(
+        <table>
+            <tbody>
+                <tr>
+                    <Row />
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+export default App;
